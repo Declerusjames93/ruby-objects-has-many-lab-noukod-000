c77593398fc39df5ba27cf704255b3369ea17c5c
James Declerus <james.declerus@esih.edu>
2019-04-29 13:40:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song
+  @@all = []
   attr_accessor :name, :artist
   def initialize(name, artist = nil)
       @name = name
@@ -7,5 +8,10 @@ class Song
 
      def artist_name
       self.artist ? self.artist.name : nil
+    end
+
+    def  self.all
+      @@all << self 
+      
     end
 end
