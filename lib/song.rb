7f536447698547a81f6117de80c8a0e5a6aad05d
James Declerus <james.declerus@esih.edu>
2019-04-29 13:35:50 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+  def initialize(name, artist = nil)
+      @name = name
+      @artist = artist
+    end
+  
+     def artist_name
+      self.artist ? self.artist.name : nil
+    end
+end
